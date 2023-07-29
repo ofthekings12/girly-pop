1901198a5eee3a2db83e38c7ae6ab3c59722694b
Mikko De Los Reyes <mikkode212@gmail.com>
2023-07-29 00:21:30 -0400

    set up react app, and created home page with button

---
+++ src/Components/Home.jsx
@@ -0,0 +1,15 @@
+import React from 'react'
+import './Home.css'
+
+
+function Home() {
+  return (
+    <div className='bg'>
+      <button className='get-girly-pop'>
+        Let's Get Girly Pop ✨
+      </button>
+    </div>
+  )
+}
+
+export default Home
--- src/Home.jsx
+++
@@ -1,9 +0,0 @@
-import React from 'react'
-
-function Home() {
-  return (
-    <div>HELLO WORLD</div>
-  )
-}
-
-export default Home
